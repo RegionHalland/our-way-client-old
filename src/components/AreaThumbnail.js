2b6a5d0e00d228973e081be82f338a51953ef5d0
Albin Martinsson <albin@digitaliseringsbyran.se>
2019-05-08 14:44:58 +0200

    Add props to areathumbnail

--- src/components/AreaThumbnail.js
+++ src/components/AreaThumbnail.js
@@ -1,17 +1,12 @@
 import { Link } from "gatsby"
 import React from "react"
 
-const AreaThumbnail = () => (
-  <div className="rounded flex flex-wrap">
-  	<div className="w-4/12 md:w-3/12 bg-grey-light rounded">
-
-  	</div>
-  	<div className="w-8/12 md:w-9/12 px-2 md:px-3">
-  		<h2 className="text-black mb-3 text-xl md:text-2xl">Stöd & Finansiering</h2>
-  		<p className="leading-tight mb-3 text-grey-dark max-w-sm">Se vilka stöd du som företagare kan få för att utveckla din verksamhet</p>
-    	<Link className="no-underline text-black font-semibold" to="/AreaSingle/">Läs mer</Link>
+const AreaThumbnail = ({title, excerpt, link, label}) => (
+  	<div className="w-full p-4 md:p-6">
+  		<h2 className="text-black mb-3 text-xl md:text-2xl">{title}</h2>
+  		<p className="leading-normal mb-3 text-grey-dark max-w-sm pr-6">{excerpt}</p>
+    	<Link className="text-black font-semibold" to="/AreaSingle/">{label}</Link>
     </div>
-  </div>
 )
 
 export default AreaThumbnail
