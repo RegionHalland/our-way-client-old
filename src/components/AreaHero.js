625f4b5c9966c4ba36171292d40da651c07cb5a1
sebastiansson <sebastian.marcusson@gmail.com>
2019-05-09 17:23:21 +0200

    Add image

--- src/components/AreaHero.js
+++ src/components/AreaHero.js
@@ -1,15 +1,21 @@
-import React from "react"
+import React from 'react'
 import styled from 'styled-components'
+import Img from 'gatsby-image/withIEPolyfill'
 
-const AreaHero = () => (
-  <div className="flex flex-wrap mb-8 md:flex-row-reverse">
-	<div className="w-full lg:w-7/12 min-h-8 bg-grey-lighter">
-		{/* TODO: Insert background image */}
+const AreaHero = ({title, image}) => (
+	<div className="flex flex-wrap mb-8 md:flex-row-reverse">
+		<div className="w-full lg:w-7/12 min-h-8 bg-grey-lighter">
+			<Img
+				fluid={image.localFile.childImageSharp.fluid}
+				objectFit="cover"
+				objectPosition="50% 50%"
+				alt={image.alt_text}>
+			</Img>
+		</div>
+		<div className="w-full lg:w-5/12 bg-blue flex py-8 lg:py-32 px-3 items-center">
+			<HeroTitle className="text-white text-4xl lg:text-5xl">{title}</HeroTitle>
+		</div>
 	</div>
-	<div className="w-full lg:w-5/12 bg-blue flex py-8 lg:py-32 px-3 items-center">
-		<HeroTitle className="text-white text-4xl lg:text-5xl">Kunskap</HeroTitle>
-	</div>
-  </div>
 )
 
 const HeroTitle = styled.h1`
--- src/templates/areaSingle.js
+++ src/templates/areaSingle.js
@@ -1,26 +1,56 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../layouts/Layout"
-import SEO from "../components/Seo"
-import AreaHero from "../components/AreaHero"
-import ArticleContent from "../components/ArticleContent"
-import Author from "../components/Author"
+import Layout from '../layouts/Layout'
+import SEO from '../components/Seo'
+import AreaHero from '../components/AreaHero'
+import ArticleContent from '../components/ArticleContent'
+import Author from '../components/Author'
 
-const AreaSingle = () => (
-  <Layout>
-    <SEO title="Områdessida" />
-    <AreaHero />
-    <div className="px-3 wrapper flex flex-wrap justify-between mb-8">
-		<div className="w-full lg:w-6/12">
-			<h2 className="mb-3">Om kunskap</h2>
-			<ArticleContent content="Vår kunskapsstrategi handlar om att samla in och sprida kunskap om den halländska gästen. Vi tar fram kunskapsunderlag som destinationer och företag kan använda som stöd för utveckling och tillväxt och som kan hjälpa dem att fatta rätt beslut i sin yrkesroll. Detta gör vi bland annat genom att presentera gästnattsstatistik, omvärldsbevaka och en gång om året presentera en turismekonomisk rapport om besöksnäringens omsättning i Halland. Tillsammans med företagen, destinationerna och turismforskningsinstitutet Etour utvecklar vi en kunskapsdatabas om den halländska besökaren. Syftet med databasen är att ge ett kunskapsunderlag som aktörerna inom besöksnäringen kan använda som stöd för utveckling och tillväxt." />
+export const query = graphql`
+	query($slug: String!) {
+		wordpressWpArea (slug: {eq: $slug}) {
+			name
+			description
+			acf {
+				image {
+					localFile {
+						childImageSharp {
+							fluid(quality: 100, maxWidth: 1200) {
+								...GatsbyImageSharpFluid_withWebp
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`
+
+const AreaSingle = ({
+	 data: { 
+		wordpressWpArea: { 
+			name,
+			description,
+			acf: {
+				image
+			}
+		}
+	}
+}) => (
+	<Layout>
+		<SEO title={name} />
+		<AreaHero title={name} image={image} />
+		<div className="px-3 wrapper flex flex-wrap justify-between mb-8">
+			<div className="w-full lg:w-6/12">
+				<h2 className="mb-3">Om {name}</h2>
+				<ArticleContent content={description} />
+			</div>
+			<div className="w-full lg:w-5/12">
+				<h2 className="mb-4">Frågor? Kontakta oss.</h2>
+				<Author />
+			</div>
 		</div>
-		<div className="w-full lg:w-5/12">
-			<h2 className="mb-4">Frågor? Kontakta oss.</h2>
-			<Author />
-		</div>
-    </div>
-  </Layout>
+	</Layout>
 )
 
 export default AreaSingle
