40067722435a92219a5d68ad81742dc337857765
Albin Martinsson <albinmartinsson91@gmail.com>
2019-05-13 13:30:36 +0200

    Update ArticleHero.js

--- src/components/ArticleHero.js
+++ src/components/ArticleHero.js
@@ -6,19 +6,19 @@ import { darken } from 'polished'
 import Moment from 'react-moment'
 
 const ArticleHero = ({title, date, label, path}) => (
-	<HeroEvent className="py-8 md:py-12 bg-blue-dark">
+	<HeroEvent className="py-8 md:py-12 bg-blue-dark block no-underline" to={path}>
 		<div className="wrapper">
 			<div className="px-3">
 				<span className="text-grey-light mb-2 block"><Moment format="YYYY/MM/DD">{date}</Moment></span>
 				<h1 className="font-semibold text-white md:text-5xl mb-4 leading-tight max-w-md">{title}</h1>
-				<Link className="text-white font-semibold" to={path}>{label}</Link>
+				<span className="text-white font-semibold">{label}</span>
 			</div>
 		</div>
 	</HeroEvent>
 )
 
 
-const HeroEvent = styled.div`
+const HeroEvent = styled(Link)`
 	background: #123843;
 	transition:background 0.25s;
 	&:hover {
