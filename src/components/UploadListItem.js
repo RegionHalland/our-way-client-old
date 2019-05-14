d9082a14ee3b0faeda1398241d8d602958c611bd
David Öhlin <hello@davidohl.in>
2019-05-14 13:20:46 +0200

    add rel="noopener noreferrer"

--- src/components/UploadListItem.js
+++ src/components/UploadListItem.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const UploadListItem = ({ resource }) => (
 	<div className="py-4 flex items-center justify-between border-t border-grey-light">
-  		<a target="_blank" className="block font-semibold text-black text-lg" href={resource.file.url.localFile.publicURL}>{resource.file.name}</a>
+  		<a target="_blank" rel="noopener noreferrer" className="block font-semibold text-black text-lg" href={resource.file.url.localFile.publicURL}>{resource.file.name}</a>
   		<span className="uppercase text-sm font-semibold text-grey-dark align-right">{resource.file.subtype} - {resource.file.url.localFile.prettySize}</span>
   	</div>
 )
