641dd293552dd1b8227bbea53d3d2e2b8974268c
Albin Martinsson <albinmartinsson91@gmail.com>
2019-05-14 09:57:57 +0200

    Update AreaUploads.js

--- src/components/AreaUploads.js
+++ src/components/AreaUploads.js
@@ -2,15 +2,18 @@ import React from "react"
 import UploadList from "./UploadList"
 
 const AreaUploads = ({ uploads }) => (
-	<div className="flex flex-wrap -mx-3">
-		{ 
-			uploads.map(list =>
-				<div className="w-full md:w-6/12 px-3 mb-6">
-					<UploadList list={list} />
-				</div>
-			)
-        }
-	</div>
+	<React.Fragment>
+		<h2 className="mb-8">Dokument & resurser</h2>
+		<div className="flex flex-wrap -mx-3">
+			{ 
+				uploads.map(list =>
+					<div className="w-full md:w-6/12 px-3 mb-6">
+						<UploadList list={list} />
+					</div>
+				)
+	        }
+		</div>
+	</React.Fragment>
 )
 
 export default AreaUploads
